5b6f5e6352dd9b7ba75ef2d22deeacb848b08ca7
Dionatan Voss <dionatan.voss@ab-inbev.com>
2023-11-16 17:41:10 -0300

    poc theme kinda works

--- packages/components/src/components/Button/Button.tsx
+++ packages/components/src/components/Button/Button.tsx
@@ -1,25 +1,45 @@
 import { ButtonHTMLAttributes, CSSProperties, PropsWithChildren } from 'react';
-
 import { Bell } from '@poc-lib/icons';
-import theme from '@poc-lib/theme';
+import theme, { css, styled } from '@poc-lib/theme';
 
 export type ButtonProps = PropsWithChildren<
   ButtonHTMLAttributes<HTMLButtonElement>
 >;
 
-const style: CSSProperties = {
-  appearance: 'none',
-  MozAppearance: 'none',
-  WebkitAppearance: 'none',
-  backgroundColor: theme.color.primitive.solid.neutral['neutral-100'].value,
-  color: theme.color.primitive.solid.neutral['neutral-0'].value,
-};
+const ButtonComponent = styled('button', {
+  base: {
+    appearance: 'none',
+    border: '1px solid blue',
+    backgroundColor: '#171717',
+    padding: '12rem',
+    borderRadius: '50%',
+  }
+})
+
+const Hr = styled('hr', {
+  base: {
+    border: '1px solid blue',
+  }
+
+}
+)
+
+// const style: CSSProperties = {
+//   appearance: 'none',
+//   MozAppearance: 'none',
+//   WebkitAppearance: 'none',
+//   backgroundColor: theme.color.primitive.solid.neutral['neutral-100'].value,
+//   color: theme.color.primitive.solid.neutral['neutral-0'].value,
+// };
 
 export function Button({ children, ...rest }: ButtonProps) {
   return (
-    <button {...rest} style={style}>
-      <Bell />
-      {children}
-    </button>
+    <div>
+      <Hr />
+      <ButtonComponent className={css({ bg: 'red' })} {...rest}>
+        <Bell />
+        {children}
+      </ButtonComponent>
+    </div>
   );
 }
--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,12 +1,16 @@
-import { border } from '../tokens/borders.json';
-import { color } from '../tokens/colors.json';
-import { font } from '../tokens/fonts.json';
-import { gradient } from '../tokens/gradients.json';
-import { measure } from '../tokens/measures.json';
-import { radius } from '../tokens/radii.json';
-import { shadow } from '../tokens/shadows.json';
-import { text } from '../tokens/text.json';
-import { typography } from '../tokens/typography.json';
+import { border } from "../tokens/borders.json";
+import { color } from "../tokens/colors.json";
+import { font } from "../tokens/fonts.json";
+import { gradient } from "../tokens/gradients.json";
+import { measure } from "../tokens/measures.json";
+import { radius } from "../tokens/radii.json";
+import { shadow } from "../tokens/shadows.json";
+import { text } from "../tokens/text.json";
+import { typography } from "../tokens/typography.json";
+
+import { css } from "@acme-org/styled-system/css";
+import { styled } from "@acme-org/styled-system/jsx";
+import "@acme-org/styled-system/styles.css";
 
 export const theme = {
   border,
@@ -21,3 +25,4 @@ export const theme = {
 };
 
 export default theme;
+export { css, styled };
